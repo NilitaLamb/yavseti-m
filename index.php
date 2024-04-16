27c0f9b3079ac1b0651b6739532bee468434dd76
NilitaLamb <165794300+NilitaLamb@users.noreply.github.com>
2024-04-16 16:55:13 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+<center>
+<input id="field_password" value="" class="fieldPassword" type="password" name="fr.password">
+  <br>
+  <input value="" id="field_login" type="text" name="fr.login">
+  <br>
+  <label for="field_password">Пароль:</label>
+  <br>
+  <input id="field_password" value="" class="fieldPassword" type="password" name="fr.password">
+</center>
+?>
